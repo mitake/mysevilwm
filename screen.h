8771796391b45c9c86bf5d58edaac279e84ecf09
Hitoshi Mitake <mitake.hitoshi@gmail.com>
2013-12-30 00:15:31 +0900

    cleaning screen

--- screen.h
+++ screen.h
@@ -1,16 +1,14 @@
 #ifndef SCREEN_H_
 #define SCREEN_H_
 
-void drag(Client *c);
-void draw_outline(Client *c);
+#include "client.h"
+
 void get_mouse_position(int *x, int *y);
 void move(Client *c, int set);
-void recalculate_sweep(Client *c, int x1, int y1, int x2, int y2);
 void resize(Client *c, int set);
 void maximise_vert(Client *c);
 void maximise_horiz(Client *c);
 void show_info(Client *c);
-void sweep(Client *c);
 void unhide(Client *c, int raise);
 void next(Client *c);
 void arrow_next(Client* c, int x, int y);
@@ -23,7 +21,4 @@ void focus(Client* c);
 void set_focus(Client* c);
 void force_set_focus(void);
 
-void change_state(Client* client,
-                  int x, int y, int w, int h, int vd, int focused, char mark);
-
-#endif // ! SCREEN_H_
+#endif /* SCREEN_H_ */
